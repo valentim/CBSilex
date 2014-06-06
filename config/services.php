c64223c3110cdc02595a5e7f75a8166849ba8e42
Thiago Valentim <thiago.valentim@clickbus.com.br>
2014-06-06 01:42:12 -0300

    added config structure

---
+++ config/services.php
@@ -0,0 +1,41 @@
+<?php
+use Clickbus\BusServiceLayer\BookingEngine\ServiceProvider;
+use Clickbus\BusServiceLayer\BookingEngine\Driver\CbConnect;
+use Clickbus\BusServiceLayer\Payment\PaymentContext;
+use Clickbus\BusServiceLayer\Payment\Adapter\CreditCardAdapter;
+use Clickbus\BusServiceLayer\Payment\Driver\BankSlip\MundiPagg as MundiPaggBankSlip;
+use Clickbus\BusServiceLayer\Payment\Driver\CreditCard\MundiPagg as MundiPaggCreditCard;
+use Clickbus\BusServiceLayer\Payment\Driver\BankTransfer\Moip;
+use Clickbus\BusServiceLayer\Payment\Adapter\BankTransferAdapter;
+
+/**
+ * Drivers of Booking Engine
+ */
+$app['booking_engine_driver_cbconnect'] = $app->share(function () {
+    return new ServiceProvider(new CbConnect);
+});
+
+/**
+ * Drivers of Payment
+ */
+$app['payment_gateway_driver_creditcard_mundipagg'] = $app->share(function () {
+
+    $driver = new MundiPaggCreditCard;
+    $adapter = new CreditCardAdapter($driver);
+
+    return new PaymentContext($adapter);
+});
+
+$app['payment_gateway_driver_banktransfer_moip'] = $app->share(function () {
+    $driver = new Moip;
+    $adapter = new BankTransferAdapter($driver);
+
+    return new PaymentContext($adapter);
+});
+
+$app['payment_gateway_driver_bankSlip_mundipagg'] = $app->share(function () {
+    $driver = new MundiPaggBankSlip;
+    $adapter = new CreditCardAdapter($driver);
+
+    return new PaymentContext($adapter);
+});
