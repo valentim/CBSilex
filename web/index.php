81b3441738286f880e75df91a099510fb19e1f14
Tiago Butzke <tiago.butzke@clickbus.com.br>
2014-06-05 23:46:19 -0300

    Adding controller route support

--- web/index.php
+++ web/index.php
@@ -1,10 +1,19 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Routing\RouteCollection;
+use Silex\Application;
+ 
+$app = new Application();
 
-$app->get('/hello/{name}', function($name) use($app) {
-        return 'Hello '.$app->escape($name);
-    });
+$app['routes'] = $app->extend('routes', function (RouteCollection $routes, Application $app) {
+    $loader = new YamlFileLoader(new FileLocator(__DIR__ . '/../app/config'));
+    $collection = $loader->load('routes.yml');
+    $routes->addCollection($collection);
+ 
+    return $routes;
+});
 
 $app->run();
