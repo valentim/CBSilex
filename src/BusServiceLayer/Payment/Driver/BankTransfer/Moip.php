68d16f4095d19d0263cd6a53031e2c6646e839a6
Thiago Valentim <thiago.valentim@clickbus.com.br>
2014-06-06 02:14:53 -0300

    implements contracts

--- src/BusServiceLayer/Payment/Driver/BankTransfer/Moip.php
+++ src/BusServiceLayer/Payment/Driver/BankTransfer/Moip.php
@@ -9,8 +9,23 @@
 namespace Clickbus\BusServiceLayer\Payment\Driver\BankTransfer;
 
 use Clickbus\BusServiceLayer\Payment\Driver\BankTransferDriver;
+use Clickbus\BusServiceLayer\Payment\PaymentTransfer;
 
 class Moip implements BankTransferDriver
 {
 
-} 
+    public function verifyPayment(PaymentTransfer $dataTransfer)
+    {
+        // TODO: Implement verifyPayment() method.
+    }
+
+    public function doPayment(PaymentTransfer $dataTransfer)
+    {
+        // TODO: Implement doPayment() method.
+    }
+
+    public function doCancelation(PaymentTransfer $dataTransfer)
+    {
+        // TODO: Implement doCancelation() method.
+    }
+}
