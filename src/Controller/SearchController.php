c456f77dc900c9baeadadf2920a031681abae400
Thiago Valentim <thiago.valentim@clickbus.com.br>
2014-06-09 14:15:27 -0300

    added search implementation

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -1,14 +1,21 @@
 <?php
 namespace Clickbus\Controller;
 
+use Clickbus\BusServiceLayer\BookingEngineService\HandlerData\DataDTO;
 use Symfony\Component\HttpFoundation\Request;
 
 use Silex\Application;
 
 class SearchController
 {
-    public function getAction(Application $app)
+    public function getAction(Application $app, Request $request)
     {
-        return $app->json(array(), 200);
+        $bookingEngine = $app['booking_engine_driver_cbconnect'];
+        $dataTransfer = new DataDTO;
+        $dataTransfer->input($request->query->all());
+
+        $response = $bookingEngine->getSearch($dataTransfer);
+
+        return $app->json($response, 200);
     }
 }
