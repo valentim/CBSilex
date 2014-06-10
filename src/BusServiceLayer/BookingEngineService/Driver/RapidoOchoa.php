3c8e0c938feb301a2540c7e5ba024705cfc27844
Thiago Valentim <thiago.valentim@clickbus.com.br>
2014-06-10 12:42:18 -0300

    added weak implementation of rapidoOchoa

--- src/BusServiceLayer/BookingEngineService/Driver/RapidoOchoa.php
+++ src/BusServiceLayer/BookingEngineService/Driver/RapidoOchoa.php
@@ -9,11 +9,12 @@
 namespace Clickbus\BusServiceLayer\BookingEngineService\Driver;
 
 
-use Clickbus\BusServiceLayer\BookingEngineService\Adaptable;
+use Camcima\Soap\Client;
 use Clickbus\BusServiceLayer\BookingEngineService\HandlerData\DataFilter;
-use Clickbus\Request\Soap\Client;
+use Clickbus\BusServiceLayer\BookingEngineService\Template;
+use Clickbus\Response\Output;
 
-class RapidoOchoa extends Adaptable
+class RapidoOchoa extends Template
 {
     public function __construct(DataFilter $filter)
     {
@@ -33,23 +34,80 @@ class RapidoOchoa extends Adaptable
         parent::__construct($filter);
     }
 
-    protected function callBooking()
+    protected function callBooking(Output $output)
     {
         // TODO: Implement callBooking() method.
     }
 
-    protected function callReserve()
+    protected function callReserve(Output $output)
     {
         // TODO: Implement callReserve() method.
     }
 
-    protected function callSeats()
+    protected function callSeats(Output $output)
     {
         // TODO: Implement callSeats() method.
     }
 
-    protected function callSearch()
+    protected function callSearch(Output $output)
     {
-        $this->client->__doRequest();
+        $xml = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
+                    <soapenv:Header>
+                      <tem:CredencialUsuario>
+                         <!--Optional:-->
+                         <tem:UserName>MVM</tem:UserName>
+                         <!--Optional:-->
+                         <tem:Password>Mvm2007mvm!</tem:Password>
+                      </tem:CredencialUsuario>
+                    </soapenv:Header>
+                    <soapenv:Body>
+                      <tem:ObtenerTarifas>
+                         <!--Optional:-->
+                         <tem:sOrigen>001</tem:sOrigen>
+                         <!--Optional:-->
+                         <tem:sDestino>029</tem:sDestino>
+                         <!--Optional:-->
+                         <tem:sFecha>2014/06/13</tem:sFecha>
+                      </tem:ObtenerTarifas>
+                    </soapenv:Body>
+                </soapenv:Envelope>';
+
+        $result = $this->client->__doRequest($xml, 'http://190.85.56.76/wsTarifasROpruebas/ServicioTarifas.asmx', 'http://tempuri.org/ObtenerTarifas', SOAP_1_2);
+
+        $loadResponse = new \SimpleXMLElement($result);
+        $obtenerTarifasResponse = current($loadResponse->xpath('child::node()'));
+        $obtenerTarifasResult = current($obtenerTarifasResponse->xpath('child::node()'));
+
+        $trips = $obtenerTarifasResult->xpath('//NewDataSet');
+
+        foreach ($trips as $trip) {
+            list($from, $to) = explode("-", $trip->Table->Ruta->__toString());
+            $set = [
+                'from' => $from,
+                'to' => $to,
+                'schedule' => $trip->Table->Fecha->__toString(),
+                'parts' => [
+                    'departure' => [
+                        'price' => $trip->Table->Tarifa->__toString()
+                    ],
+                    'arrival' => [
+                        'price' => null
+                    ],
+                    'bus' => [
+                        'service_class' => $trip->Table->Servicio->__toString()
+                    ]
+                ],
+                'availableSeats' => $trip->Table->PuestosLibres->__toString()
+            ];
+            array_push($this->template['items'], $set);
+        }
+
+        $output->setOutput($this->template);
+
+    }
+
+    protected function getData()
+    {
+        // TODO: Implement getData() method.
     }
 }
