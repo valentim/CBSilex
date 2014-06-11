754fe3b4570d55fb8a057aceb7715624463aeb93
Tiago Butzke <tiago.butzke@clickbus.com.br>
2014-06-11 15:43:33 -0300

    Providing driver via service

--- config/environment/dev.php
+++ config/environment/dev.php
@@ -1,9 +1,9 @@
 <?php
-use Symfony\Component\Debug\ErrorHandler;
+// use Symfony\Component\Debug\ErrorHandler;
 
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 $app['debug'] = true;
 
-ErrorHandler::register();
+// ErrorHandler::register();
--- config/services.php
+++ config/services.php
@@ -4,12 +4,13 @@ use Clickbus\BusServiceLayer\BookingEngineService\Driver\RapidoOchoa;
 use Clickbus\BusServiceLayer\BookingEngineService\Service\ServiceProvider;
 use Clickbus\BusServiceLayer\PaymentService\PaymentContext;
 use Clickbus\BusServiceLayer\PaymentService\Adapter\CreditCardAdapter;
-use Clickbus\BusServiceLayer\PaymentService\Driver\BankSlip\MundiPagg as MundiPaggBankSlip;
-use Clickbus\BusServiceLayer\PaymentService\Driver\CreditCard\MundiPagg as MundiPaggCreditCard;
-use Clickbus\BusServiceLayer\PaymentService\Driver\BankTransfer\Moip;
+// use Clickbus\BusServiceLayer\PaymentService\Driver\BankSlip\MundiPagg as MundiPaggBankSlip;
+// use Clickbus\BusServiceLayer\PaymentService\Driver\CreditCard\MundiPagg as MundiPaggCreditCard;
+// use Clickbus\BusServiceLayer\PaymentService\Driver\BankTransfer\Moip;
 use Clickbus\BusServiceLayer\PaymentService\Adapter\BankTransferAdapter;
 use Clickbus\BusServiceLayer\PaymentService\Adapter\BankSlipAdapter;
 use Clickbus\BusServiceLayer\BookingEngineService\HandlerData\Intersection;
+use Clickbus\BusServiceLayer\PaymentService\Provider\PaymentDriverServiceProvider;
 
 use DerAlex\Silex\YamlConfigServiceProvider;
 
@@ -33,30 +34,6 @@ $app['booking_engine_driver_rapidoochoa'] = $app->share(function () {
 });
 
 /**
- * Drivers of PaymentService
- */
-$app['payment_gateway_driver_creditcard_mundipagg'] = $app->share(function () {
-    $driver = new MundiPaggCreditCard();
-    $adapter = new CreditCardAdapter($driver);
-
-    return new PaymentContext($adapter);
-});
-
-$app['payment_gateway_driver_banktransfer_moip'] = $app->share(function () {
-    $driver = new Moip();
-    $adapter = new BankTransferAdapter($driver);
-
-    return new PaymentContext($adapter);
-});
-
-$app['payment_gateway_driver_bankSlip_mundipagg'] = $app->share(function () {
-    $driver = new MundiPaggBankSlip();
-    $adapter = new BankSlipAdapter($driver);
-
-    return new PaymentContext($adapter);
-});
-
-/**
  * Registering country from call
  */
 $app->register(new CountryServiceProvider());
@@ -65,6 +42,11 @@ $app->register(new CountryServiceProvider());
  * Registering Yaml service provider
  */
 $app->register(new YamlConfigServiceProvider(__DIR__ . '/parameters.yml'));
+
+/**
+ * Drivers of PaymentService
+ */
+$app->register(new PaymentDriverServiceProvider());
 
 /**
  * Doctrine services
