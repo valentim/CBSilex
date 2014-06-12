9c15d29595d8fefb0b9fc40e437afa345ff0beb9
Tiago Butzke <tiago.butzke@clickbus.com.br>
2014-06-12 12:02:20 -0300

    PaymentService to another repository

--- src/BusServiceLayer/PaymentService/Provider/NotExistsServiceException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Clickbus\BusServiceLayer\PaymentService\Driver\Provider;
-
-use Clickbus\Exception\GenericCallbackMessageException;
-
-class NotExistsServiceException extends GenericCallbackMessageException {}
