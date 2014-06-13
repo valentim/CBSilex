9e86872aba62709d6a112eec0b44a13380ce92ac
Thiago Valentim <thiago.valentim@clickbus.com.br>
2014-06-13 01:59:47 -0300

    added interface sufix

---
+++ src/BusServiceLayer/BookingEngineService/ExternalProviderInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thiagovalentim
+ * Date: 6/5/14
+ * Time: 17:22
+ */
+
+namespace Clickbus\BusServiceLayer\BookingEngineService;
+
+
+use Clickbus\BusServiceLayer\DataTransfer\TransferInterface;
+
+interface ExternalProviderInterface
+{
+    public function getSearch(TransferInterface $searchTransfer);
+    public function getSeats(TransferInterface $searchTransfer);
+    public function reserve(TransferInterface $searchTransfer);
+    public function doBooking(TransferInterface $searchTransfer);
+
+} 
