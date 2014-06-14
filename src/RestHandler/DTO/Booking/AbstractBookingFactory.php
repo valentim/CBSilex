af643953c9f0d92ad7302e7e220163aa882d535e
Tiago Butzke <tiago.butzke@clickbus.com.br>
2014-06-14 04:55:11 -0300

    Booking factory

--- src/RestHandler/DTO/Booking/AbstractBookingFactory.php
+++ src/RestHandler/DTO/Booking/AbstractBookingFactory.php
@@ -1,14 +1,138 @@
 <?php
 namespace Clickbus\RestHandler\DTO\Booking;
 
+use Clickbus\RestHandler\DTO\Booking\Meta;
+use Clickbus\RestHandler\DTO\Booking\Payment;
+use Clickbus\RestHandler\DTO\Booking\Schedule;
+use Clickbus\RestHandler\DTO\Booking\Departure;
+use Clickbus\RestHandler\DTO\Booking\Arrival;
+use Clickbus\RestHandler\DTO\Booking\Type;
+use Clickbus\RestHandler\DTO\Booking\Seat;
+use Clickbus\RestHandler\DTO\Booking\Passenger;
+use Clickbus\RestHandler\DTO\Booking\Product;
+use Clickbus\RestHandler\DTO\Booking\Item;
 use Clickbus\RestHandler\DTO\Booking\Booking;
 use Clickbus\RestHandler\DTO\Booking\BookingDelete;
 use Clickbus\RestHandler\DTO\Booking\DeleteItem;
 
 class AbstractBookingFactory
 {
-    public static function buildBooking()
+    public static function buildBooking(
+        $id,
+        $status,
+        $localizer,
+        $uuid,
+        $payment,
+        $createdAt,
+        $card,
+        $code,
+        $name,
+        $expiration,
+        $paymentMethod,
+        $paymentTotal,
+        $paymentCurrency,
+        $paymentStatus,
+        $typeName,
+        $typeDicount,
+        $typeId,
+        array $items
+    )
     {
+        $meta = new Meta();
+        $meta->setCard($card);
+        $meta->setCode($code);
+        $meta->setName($name);
+        $meta->setExpiration($expiration);
+
+        $payment = new Payment();
+        $payment->setMethod($paymentMethod);
+        $payment->setTotal($paymentTotal);
+        $payment->setCurrency($paymentCurrency);
+        $payment->setStatus($paymentStatus);
+        $payment->setMeta($meta);
+
+        $type = new Type();
+        $type->setName($typeName);
+        $type->setDicount($typeDicount);
+        $type->setId($typeId);
+
+        $booking = new Booking();
+        $booking->setId($id);
+        $booking->setStatus($status);
+        $booking->setLocalizer($localizer);
+        $booking->setUuid($uuid);
+        $booking->setPayment($payment);
+        $booking->setCreatedAt($createdAt);
+
+        if (count($items) > 0) {
+            foreach ($items as $item) {
+                $bookingItem = new Item();
+                $bookingItem->setTripId($item['tripId']);
+                $bookingItem->setOrderItem($item['orderItem']);
+
+                $departureSchedule = new Schedule();
+                $departureSchedule->setId($item['departure']['schedule']['id']);
+                $departureSchedule->setDate($item['departure']['schedule']['date']);
+                $departureSchedule->setTime($item['departure']['schedule']['time']);
+                $departureSchedule->setTimezone($item['departure']['schedule']['timezone']);
+                $departure = new Departure();
+                $departure->setWaypoint($item['departure']['waypoint']);
+                $departure->setSchedule($departureSchedule);
+
+                $arrivalSchedule = new Schedule();
+                $arrivalSchedule->setId($item['arrival']['schedule']['id']);
+                $arrivalSchedule->setDate($item['arrival']['schedule']['date']);
+                $arrivalSchedule->setTime($item['arrival']['schedule']['time']);
+                $arrivalSchedule->setTimezone($item['arrival']['schedule']['timezone']);
+                $arrival = new Arrival();
+                $arrival->setWaypoint($item['arrival']['waypoint']);
+                $arrival->setSchedule($arrivalSchedule);
+
+                $type = new Type();
+                $type->setName($item['type']['id']);
+                $type->setDicount($item['type']['discount']);
+                $type->setId($item['type']['id']);
+
+                $seat = new Seat();
+                $seat->setId($item['seat']['id']);
+                $seat->setName($item['seat']['name']);
+                $seat->setPrice($item['seat']['price']);
+                $seat->setStatus($item['seat']['status']);
+                $seat->setCurrency($item['seat']['currency']);
+                $seat->setType($type);
+
+                $passenger = new Passenger();
+                $passenger->setFirstName($item['passenger']['firstName']);
+                $passenger->setLastName($item['passenger']['lastName']);
+                $passenger->setEmail($item['passenger']['email']);
+                $passenger->setDocument($item['passenger']['currency']);
+                $passenger->setGender($item['passenger']['gender']);
+                $passenger->setBirthday($item['passenger']['birthday']);
+                $passenger->setMeta($item['passenger']['meta']);
+
+                $bookingItem->setDeparture($departure);
+                $bookingItem->setArrival($arrival);
+                $bookingItem->setSeat($seat);
+                $bookingItem->setPassenger($passenger);
+                $bookingItem->setSubtotal($item['subtotal']);
+
+                if (count($item['products']) > 0) {
+                    foreach ($item['products'] as $product) {
+                        $bookingProduct = new Product();
+                        $bookingProduct->setUuid($product['uuid']);
+                        $bookingProduct->setName($product['name']);
+                        $bookingProduct->setQuantity($product['quantity']);
+                        $bookingProduct->setPrice($product['price']);
+                        $bookingProduct->setCurrency($product['currency']);
+                        $bookingItem->addProduct($bookingProduct);
+                    }
+                }
+
+                $booking->addItem($bookingItem);
+            }
+        }
+
+        return $booking;
     }
 
     public static function buildBookingDelete(
--- tests/RestHandler/DTO/Booking/AbstractBookingFactoryTest.php
+++ tests/RestHandler/DTO/Booking/AbstractBookingFactoryTest.php
@@ -5,6 +5,105 @@ use Clickbus\RestHandler\DTO\Booking\AbstractBookingFactory;
 
 class AbstractBookingFactoryMethod extends \PHPUnit_Framework_TestCase
 {
+    public function testBuildBooking()
+    {
+        $id = 123;
+        $status = 'pending';
+        $localizer = 'ABDDDD999';
+        $uuid = '53347e09aee47';
+        $payment = $payment;
+        $createdAt = '2010-10-30';
+        $card = 'XXXX-XXXX-XXXX-1234';
+        $code = 'XXX';
+        $name = 'Klederson Bueno Bezerra da Silva';
+        $expiration = 'XXXX-XX-XX';
+        $paymentMethod = 'Credit Card';
+        $paymentTotal = 3900;
+        $paymentCurrency = 'BRL';
+        $paymentStatus = 'pending';
+        $typeName = 'Professor';
+        $typeDicount = 0.9;
+        $typeId = 1;
+        $items = array(
+            array(
+                'tripId' => 123123123,
+                'orderItem' => 1234,
+                'departure' => array(
+                    'schedule' => array(
+                        'id' => 15,
+                        'date' => '2014-10-31',
+                        'time' => '23:00',
+                        'timezone' => 'America/Sao_Paulo',
+                    ),
+                    'waypoint' => 113,
+                ),
+                'arrival' => array(
+                    'schedule' => array(
+                        'id' => 15,
+                        'date' => '2014-10-31',
+                        'time' => '23:00',
+                        'timezone' => 'America/Sao_Paulo',
+                    ),
+                    'waypoint' => 123,
+                ),
+                'type' => array(
+                    'name' => 'Professor',
+                    'dicount' => 0.9,
+                    'id' => 1,
+                ),
+                'seat' => array(
+                    'id' => 14,
+                    'name' => 'A01',
+                    'price' => 1000,
+                    'status' => 'pending',
+                    'currency' => 'BRL',
+                ),
+                'passenger' => array(
+                    'firstName' => 'Klederson',
+                    'lastName' => 'Bueno',
+                    'email' => 'dev@clickbus.com.br',
+                    'document' => '123.123.123-01',
+                    'gender' => 'M',
+                    'birthday' => '1986-05-17',
+                    'meta' => array()
+                ),
+                'products' => array(
+                    array(
+                        'uuid' => 'abcd123s',
+                        'name' => 'Potato Chips',
+                        'quantity' => 2,
+                        'price' => 500,
+                        'currency' => 'BRL',
+                    )
+                ),
+                'subtotal' => 1900,
+            )
+        );
+
+        $booking = AbstractBookingFactory::buildBooking(
+            $id,
+            $status,
+            $localizer,
+            $uuid,
+            $payment,
+            $createdAt,
+            $card,
+            $code,
+            $name,
+            $expiration,
+            $paymentMethod,
+            $paymentTotal,
+            $paymentCurrency,
+            $paymentStatus,
+            $typeName,
+            $typeDicount,
+            $typeId,
+            $items
+        );
+
+        $this->assertInstanceOf('Clickbus\RestHandler\DTO\Booking\Booking', $booking);
+    }
+
     public function testBuildBookingDelete()
     {
         $card = 'XXXX-XXXX-XXXX-1234';
@@ -31,8 +130,24 @@ class AbstractBookingFactoryMethod extends \PHPUnit_Framework_TestCase
         $status = 'canceled';
         $localizer = 'ABDDDD999';
 
-        $booking = AbstractBookingFactory::buildBookingDelete($id, $status, $localizer, $paymentMethod, $paymentTotal, $paymentCurrency, $paymentStauts, $items, $refundType, $refundTotal, $card, $code, $name, $expiration);
+        $booking = AbstractBookingFactory::buildBookingDelete(
+            $id,
+            $status,
+            $localizer,
+            $paymentMethod,
+            $paymentTotal,
+            $paymentCurrency,
+            $paymentStauts,
+            $items,
+            $refundType,
+            $refundTotal,
+            $card,
+            $code,
+            $name,
+            $expiration
+        );
 
-        $this->assertInstanceOf('Clickbus\RestHandler\DTO\Booking\BookingDelete', $booking);
+        $this->assertInstanceOf('Clickbus\RestHandler\DTO\Booking\BookingDelete',
+            $booking);
     }
 }
