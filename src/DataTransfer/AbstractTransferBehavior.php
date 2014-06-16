c6973aeee34ef2ca4154424d2bc3e3847c26eae3
Thiago Valentim <thiago.valentim@clickbus.com.br>
2014-06-16 00:48:13 -0300

    added json serializable behavior

--- src/DataTransfer/AbstractTransferBehavior.php
+++ src/DataTransfer/AbstractTransferBehavior.php
@@ -9,7 +9,9 @@
 namespace Clickbus\DataTransfer;
 
 
-abstract class AbstractTransferBehavior implements TransferInterface
+use SplObjectStorage;
+
+abstract class AbstractTransferBehavior implements TransferInterface, \JsonSerializable
 {
     public function getData()
     {
@@ -22,5 +24,38 @@ abstract class AbstractTransferBehavior implements TransferInterface
         array_pop($namespace);
 
         return implode("\\", $namespace);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.4.0)<br/>
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     */
+    public function jsonSerialize()
+    {
+        $properties = get_object_vars($this);
+
+        $data = [];
+        foreach ($properties as $property => $value) {
+            if ($value instanceof SplObjectStorage) {
+                $value = $this->getCollection($value);
+            }
+            $data[$property] = $value;
+        }
+
+        return $data;
+    }
+
+    protected function getCollection($values)
+    {
+        $collection = [];
+
+        foreach ($values as $value) {
+            array_push($collection, $value);
+        }
+
+        return $collection;
     }
 }
