0765625cf19bec4cd621cf460ad32dc229cde1d0
Thiago Valentim <thiago.valentim@clickbus.com.br>
2014-06-16 00:46:24 -0300

    added bootstraping of tests

---
+++ web/test.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Silex\Application;
+
+$app = new Application();
+
+
+require __DIR__ . '/../config/environment/dev.php';
+require __DIR__ . '/../config/services.php';
+require __DIR__ . '/../config/routes.php';
+require __DIR__ . '/../config/errors.php';
+
+return $app;
