017d13dad134b69b701699bbcdf9df57cdbd18a6
Thiago Valentim <thiago.valentim@clickbus.com.br>
2014-06-17 19:36:08 -0300

    added portfolio container

---
+++ src/RestHandler/DataTransfer/Response/Trip/PortfolioContainer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thiagovalentim
+ * Date: 6/16/14
+ * Time: 18:44
+ */
+
+namespace Clickbus\RestHandler\DataTransfer\Response\Trip;
+
+
+use Clickbus\RestHandler\DataTransfer\AbstractTransferBehavior;
+
+class PortfolioContainer extends AbstractTransferBehavior
+{
+    protected $meta;
+    protected $content;
+
+    public function __construct()
+    {
+        $this->content = new \SplObjectStorage;
+    }
+
+    /**
+     * @param mixed $meta
+     */
+    public function setMeta($meta)
+    {
+        $this->meta = $meta;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMeta()
+    {
+        return $this->meta;
+    }
+
+    /**
+     * @param mixed $items
+     */
+    public function setContent(Trip $trip)
+    {
+        $this->content->attach($trip);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+} 
