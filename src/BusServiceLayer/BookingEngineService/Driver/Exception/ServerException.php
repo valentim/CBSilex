97061ffb6a7a214749cc860a4979fe12b713ca96
Thiago Valentim <thiago.valentim@clickbus.com.br>
2014-06-16 22:09:46 -0300

    added driver exceptions

---
+++ src/BusServiceLayer/BookingEngineService/Driver/Exception/ServerException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thiagovalentim
+ * Date: 6/9/14
+ * Time: 11:50
+ */
+
+namespace Clickbus\BusServiceLayer\BookingEngineService\Driver\Exception;
+
+use Clickbus\Exception\GenericCallbackMessageException;
+
+class ServerException extends GenericCallbackMessageException {}
