72faadf9bfb16e68456b50f01b71b901fb8541b2
Thiago Valentim <thiago.valentim@clickbus.com.br>
2014-06-25 17:47:40 -0300

    register Doctrine Provider

--- config/services.php
+++ config/services.php
@@ -1,7 +1,8 @@
 <?php
-use Clickbus\BusServiceLayer\BookingEngineService\HandlerData\Intersection;
 use Clickbus\BusServiceLayer\BookingEngineService\Service\DriverServiceProvider;
 use Clickbus\BusServiceLayer\PaymentService\Provider\PaymentDriverServiceProvider;
+use Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 
 use DerAlex\Silex\YamlConfigServiceProvider;
@@ -21,6 +22,29 @@ $app->register(new DriverServiceProvider);
  */
 $app->register(new PaymentDriverServiceProvider);
 
+
+// Register Doctrine ORM
+$app->register(new DoctrineServiceProvider, array(
+    "db.options"    => array(
+        "driver"    => "pdo_mysql",
+        "dbname"    => "newplataform",
+        "host"      => "localhost",
+        "user"      => "root",
+        "password"  => "root"
+    ),
+));
+
+$app->register(new DoctrineOrmServiceProvider, array(
+    "orm.em.options" => array(
+        "mappings" => array(
+            array(
+                "type" => "annotation",
+                "namespace" => "Clickbus\Entity",
+                "path" => __DIR__."/../src/Entity",
+            )
+        ),
+    ),
+));
 
 $app->before(function (Request $request) {
     if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
